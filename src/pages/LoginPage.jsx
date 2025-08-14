e8b17eed14e6d104f5de64a075393e757689e512
AbdelFadel06 <abdelfadelsaliou@gmail.com>
2025-08-14 10:56:45 +0100

    correct1

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = ()=>{
     
     return (
         <div className="bg-[#A7D7C5] min-h-screen flex items-center justify-center">
-            <form className="bg-[#F6FBF9] w-[400px] p-8 rounded-2xl text-center shadow">
+            <form className="bg-[#F6FBF9] w-[400px] p-8 rounded-2xl text-center shadow" onSubmit={handleLogin}>
                 <h1 className="text-2xl font-bold mb-2">Login</h1>
                 <p className="text-sm mb-6 text-gray-600">
                 Login to your account to enjoy all the services without any ads for free
@@ -50,7 +50,7 @@ const LoginPage = ()=>{
                 />
 
                 
-                <button className="w-1/2 py-4 bg-[#84C7AE] text-white text-lg font-medium rounded cursor-pointer hover:bg-[#6bbfa1] transition" onClick={handleLogin}>
+                <button className="w-1/2 py-4 bg-[#84C7AE] text-white text-lg font-medium rounded cursor-pointer hover:bg-[#6bbfa1] transition" type="submit">
                     Log In
                 </button>
 
